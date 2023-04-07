8f563d74b7868172f6b041c443917699abeee8e9
daye <dykim315@naver.com>
2023-04-07 14:21:36 +0900

    Initial Commit

---
+++ GO_SOPT_Seminar_Assingment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GO_SOPT_Seminar_Assingment
+//
+//  Created by 김다예 on 2023/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
